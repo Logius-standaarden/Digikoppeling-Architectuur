f3c6c5f333db3b9428fbadb2e2e778630b503e20
Alexander Green <alexander.green@logius.nl>
2025-09-17 10:25:29 +0200

    Beheer: geen hard-coded bestandsnaam

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Architectuur.pdf"
-      } ],
   editors: [ 
       { 
         "company" : "Logius",
